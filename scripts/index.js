21c5c5081f1c0e708553d3b7012e9ed710b8a185
Maxim Chalikov <aracctur@yandex.ru>
2024-09-23 16:36:47 +0300

    feat: объявлена функция создания карточки с кнопкой удаления

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,22 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+
+function createCard(title, url) {
+  const card = cardTemplate.cloneNode(true);
+  const cardImage = card.querySelector('.card__image');
+  const cardTitle = card.querySelector('.card__title');
+  const buttonDelete = card.querySelector('.card__delete-button')
+  cardImage.src = url;
+  cardImage.alt = title;
+  cardTitle.textContent = title;
+  buttonDelete.addEventListener('click', (evt) => {
+    evt.target.parentElement.remove();
+  })
+  console.log(card);
+  return card;
+}
+
+createCard('111', '222')
